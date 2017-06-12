edb2e751e1c2c37dfb1d621eb1bee29bc0f320f4
wanggaixin <wanggaixin@syswin.com>
2017-06-12 15:39:25 +0800

    修改

--- src/components/subComponents/Top1header.vue
+++ src/components/subComponents/Top1header.vue
@@ -7,8 +7,13 @@
     <div>
       <mu-drawer :open="open" :docked="docked" @close="toggle()">
         <mu-list @itemClick="docked ? '' : toggle()">
-          <mu-list-item title="登录">
+          <mu-list-item title="登录" @click="moduleClick">
             <mu-icon value="person_pin" slot="left" color="pinkA200"/>
+            <mu-icon value="keyboard_arrow_right" slot="right" color="pinkA200"/>
+          </mu-list-item>
+          <mu-divider />
+          <mu-list-item title="时效查询" @click="moduleClick">
+            <mu-icon value="alarm" slot="left" color="pinkA200"/>
             <mu-icon value="keyboard_arrow_right" slot="right" color="pinkA200"/>
           </mu-list-item>
           <mu-divider />
@@ -22,6 +27,7 @@
         </mu-list>
       </mu-drawer>
     </div>
+    <mu-toast v-if="toast" message="组件未完成" />
   </div>
 </template>
 
@@ -30,22 +36,30 @@
       data(){
         return {
           open: false,
-          docked: true
+          docked: true,
+          toast:false
         }
       },
       methods: {
         toggle (flag) {
           this.open = !this.open
           this.docked = !flag
+        },
+      moduleClick(){
+          this.toast=true;
+          if (this.toastTimer) clearTimeout(this.toastTimer)
+          this.toastTimer = setTimeout(() => { this.toast = false }, 2000)
         }
       }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
+<style lang="less" scoped>
+  .mu-toast{
+    left: 35%;
+    width: auto;
+    bottom: 50%;
+  }
 
 </style>
